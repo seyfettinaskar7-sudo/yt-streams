0893f7a2712ecb4e9436ad706e4b398d16ed7419
seyfettinaskar7-sudo <seyfettinaskar7@gmail.com>
2026-06-25 10:28:29 +0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -30,9 +30,9 @@ kanallar = [
     ("Tv_Net", "TV Net", "https://www.youtube.com/@tvnet/live"),
     ("Bein_Spor_Haber", "Bein Spor Haber", "https://www.youtube.com/@beINSPORTST%C3%BCrkiye/live"),
     ("Bizim_Ev_Tv", "Bizimev TV", "https://www.youtube.com/@bizimevtv2000/live"),
-    ("CNBC_E", "CNBC-e", "https://www.youtube.com/@cnbce/live")
-    ("Diyanet_Cocuk", "Diyanet Çocuk", "https://www.youtube.com/@DiyanetCocuk/live")
-    ("Kemal_Sunal_Tv", "Kemal Sunal TV", "https://www.youtube.com/@GulsahFilmOfficial/live")
+    ("CNBC_E", "CNBC-e", "https://www.youtube.com/@cnbce/live"),
+    ("Diyanet_Cocuk", "Diyanet Çocuk", "https://www.youtube.com/@DiyanetCocuk/live"),
+    ("Kemal_Sunal_Tv", "Kemal Sunal TV", "https://www.youtube.com/@GulsahFilmOfficial/live"),
 ]
 
 streams_dir = "streams"
